794dc1f13a0675765d6528e681b71fc1f4bc11b5
DKeshavarz <dankeshavarz1075@gmail.com>
2024-02-05 14:40:02 +0330

    first_commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "nigger" ;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
