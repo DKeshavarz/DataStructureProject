fcd7d3695d37d256a0159e41499d6c304edac890
Fateme Haseli <109644335+fatemeHaseli@users.noreply.github.com>
2024-02-13 17:11:10 +0330

    New time class

--- time.cpp
+++ time.cpp
@@ -1,52 +1,135 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
-class Time {
-private:
-    int hour;
-    int minute;
-    std::string period;
+using namespace std;
 
-public:
-    Time(int h, int m, const std::string& p) : hour(h), minute(m), period(p) {}
-
-    int getHour() const {
-        return hour;
-    }
-
-    int getMinute() const {
-        return minute;
-    }
-
-    std::string getPeriod() const {
-        return period;
-    }
+struct tClock
+{
+    unsigned int hour = 0;
+    unsigned int min = 0;
+    string dayORnight = "AM";
 };
 
-class Clock {
-private:
-    std::vector<Time> times;
+class time
+{
+    private:
 
-public:
-    void addTime(const Time& t) {
-        times.push_back(t);
-    }
+        vector <struct tClock> times;
 
-    Time getTime(int index) const {
-        if (index >= 0 && index < times.size()) {
-            return times[index];
-        } else {
-            return Time(-1, -1, "AM");
-        }
-    }
+        struct tClock temp;
+    
+    public:
 
-    bool isTimeExist(int hour, int minute, const std::string& period) const {
-        for (const auto& time : times) {
-            if (time.getHour() == hour && time.getMinute() == minute && time.getPeriod() == period) {
+        bool set_hour (unsigned int hour)
+        {
+            if (hour >= 1 && hour <= 12)
+            {
+                this->temp.hour = hour;
+
                 return true;
             }
+            else
+            {
+                return false;
+            }
         }
-        return false;
-    }
+
+        unsigned int get_hour (unsigned int index)
+        {
+            return times.at (index).hour;
+        }
+
+        bool set_min (unsigned int min)
+        {
+            if (min >= 0 && min <= 59)
+            {
+                this->temp.min = min;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        unsigned int get_min (unsigned int index)
+        {
+            return times.at (index).min;
+        }
+
+        bool set_dayORnight (string dayORnight)
+        {
+            if (dayORnight == "AM" || dayORnight == "PM")
+            {
+                this->temp.dayORnight = dayORnight;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        string get_dayORnight (unsigned int index)
+        {
+            return times.at (index).dayORnight;
+        }
+
+        bool addClock (string clock)
+        {
+            string hour;
+            hour += clock.at (0);
+            hour += clock.at (1);
+
+            string min;
+            min += clock.at (3);
+            min += clock.at (4);
+
+            string dayORnight;
+            dayORnight += clock.at (6);
+            dayORnight += clock.at (7);
+
+            unsigned int tHour = atoi (hour.c_str ());
+            unsigned int tMin = atoi (min.c_str ());
+
+            if (this->set_hour (tHour) && this->set_min (tMin) && this->set_dayORnight (dayORnight))
+            {
+                this->times.push_back (temp);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool isTimeExist (string clock)
+        {
+            string hour;
+            hour += clock.at (0);
+            hour += clock.at (1);
+
+            string min;
+            min += clock.at (3);
+            min += clock.at (4);
+
+            string dayORnight;
+            dayORnight += clock.at (6);
+            dayORnight += clock.at (7);
+
+            unsigned int tHour = atoi (hour.c_str ());
+            unsigned int tMin = atoi (min.c_str ());
+
+            for (unsigned int i = 0; i < times.size (); i++)
+            {
+                if (times.at (i).hour == tHour && times.at (i).min == tMin && times.at (i).dayORnight == dayORnight)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 };
