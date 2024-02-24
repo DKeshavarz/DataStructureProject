0850361b389eaee2d2a9805b4f8784d5320a11fa
DKeshavarz <dankeshavarz1075@gmail.com>
2024-02-24 20:30:50 +0330

    [200~

--- NodeInfo.cpp
+++ NodeInfo.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 NodeInfo::NodeInfo()
-:vis(false),cost(INT_MAX),distance(INT_MAX)
+:vis(false),cost(INT_MAX),distance(INT_MAX),parent("nothing"),nodeVehicle(nullptr)
 {
 
 }
--- NodeInfo.h
+++ NodeInfo.h
@@ -3,20 +3,24 @@
 #ifndef NODEINFO_H
 #define NODEINFO_H
 
+class Vehichle;
+
 class NodeInfo 
 {
     public:
         NodeInfo();
 
-        void setVis      (const bool        input) {this->vis      = input;} 
-        void setCost     (const int         input) {this->cost     = input;}
-        void setDistance (const int         input) {this->distance = input;}
-        void setParent   (const std::string input) {this->parent   = input;}    
+        void setVis        (const bool        input) {this->vis         = input;} 
+        void setCost       (const int         input) {this->cost        = input;}//tip:get by refrence of what??
+        void setDistance   (const int         input) {this->distance    = input;}
+        void setParent     (const std::string input) {this->parent      = input;}   
+        void setNodeVehicle(Vehichle* const   input) {this->nodeVehicle = input;}
 
-        bool        getVis      ()const {return this->vis     ;}
-        int         getCost     ()const {return this->cost    ;}
-        int         getDistance ()const {return this->distance;}
-        std::string getParent   ()const {return this->parent  ;}
+        bool        getVis        ()const {return this->vis     ;}
+        int         getCost       ()const {return this->cost    ;}
+        int         getDistance   ()const {return this->distance;}//tip:pass by refrence or what???
+        std::string getParent     ()const {return this->parent  ;}
+        Vehichle*   getNodeVehicle()const {return this->nodeVehicle;}
 
         std::string print()const;
     private:
@@ -25,6 +29,7 @@ class NodeInfo
         int  distance;
 
         std::string parent;
+        Vehichle* nodeVehicle;//todo: better if ptr to const
 };
 
 #endif
