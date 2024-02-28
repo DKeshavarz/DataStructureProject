42f051fc000124d4f35011aaa50d9113f90520fd
sara <sararashedi@gmail.com>
2024-02-27 18:39:27 -0800

    delete extra files and add some newfiles and addclick on station

--- U_M/SMHANHasht/main.cpp
+++ U_M/SMHANHasht/main.cpp
@@ -1,11 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "sahman.h"
-#include <QQmlContext>
+
 
 int main(int argc, char *argv[])
 {
-    Sahman *sahmanhasht=new Sahman;
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("back",sahmanhasht);
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
---
+++ U_M/SMHANHasht/main.qml
@@ -0,0 +1,31 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+import QtQuick.Controls 2.5
+import QtQuick.VirtualKeyboard 2.4
+
+Window {
+    id: root
+    width: 1980
+    height: 1080
+    visible: true
+    title: qsTr("Hello World")
+    Rectangle
+    {
+
+        anchors.fill:parent
+        color:"pink"
+        MouseArea
+        {
+            anchors.fill:parent
+            onClicked:
+            {
+
+                var component=Qt.createComponent("M_window.qml");
+                var window = component.createObject(root);
+                window.show()
+
+            }
+        }
+    }
+
+ }
