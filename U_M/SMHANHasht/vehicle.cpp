e7467c11b7d909338f1b0c5e781b51b4b012a27f
DKeshavarz <dankeshavarz1075@gmail.com>
2024-03-01 19:43:55 +0330

    class vehcle now calculate with heap

--- U_M/SMHANHasht/vehicle.cpp
+++ U_M/SMHANHasht/vehicle.cpp
@@ -51,7 +51,7 @@ bool Vehicle::readFile(const string& fileName) //add execption
     return true; //reading file was successfull
 }
 
-void Vehicle::calculateMinDistance(unordered_map<string,NodeInfo>& table, const std::string& srcNode )
+void Vehicle::calculateMinDistance(unordered_map<string,NodeInfo>& table,priority_queue<NodeNeighbour,vector<NodeNeighbour>,greater<NodeNeighbour>>&minHeap, const std::string& srcNode )
 {
     unordered_set<NodeNeighbour,NodeNeighbour::myHash> distanceSet;
     distanceFromSrc(distanceSet,srcNode);
@@ -65,6 +65,7 @@ void Vehicle::calculateMinDistance(unordered_map<string,NodeInfo>& table, const 
             table[currentNode.nodeName].setNodeVehicle(this);
             table[currentNode.nodeName].setCost(table[srcNode].getCost() + calculateCost(currentNode.distance));
             table[currentNode.nodeName].setTime(Time(table[srcNode].getTimeInt() + calculateTime(currentNode.distance)));
+            minHeap.push({currentNode.nodeName,table[srcNode].getDistance() + currentNode.distance});
         }
 }
 void Vehicle::distanceFromSrc(unordered_set<NodeNeighbour,NodeNeighbour::myHash>& distanceSet,const string& srcNode)
@@ -90,31 +91,29 @@ void Vehicle::distanceFromSrc(unordered_set<NodeNeighbour,NodeNeighbour::myHash>
     }
 }
 
-void Vehicle::calculateMinTime(unordered_map<string,NodeInfo>& table, const std::string& srcNode )
+void Vehicle::calculateMinTime(unordered_map<string,NodeInfo>& table,priority_queue<NodeNeighbour,vector<NodeNeighbour>,greater<NodeNeighbour>>&minHeap, const std::string& srcNode )
 {
     unordered_set<NodeNeighbour,NodeNeighbour::myHash> distanceSet;
     distanceFromSrc(distanceSet,srcNode);
 
 
-    cout << this->fileName << "*******************************\n";
     for(const auto &currentNode : distanceSet)
     {
-        cout << "end is:" << currentNode.nodeName <<" ->current" << table[currentNode.nodeName].getTime() << "  altrernative : " <<
-                table[srcNode].getTime() << " + " <<  calculateTime(currentNode.distance) << "distance" <<currentNode.distance <<'\n';
+
         if(!table[currentNode.nodeName].getVis() &&
             table[currentNode.nodeName].getTimeInt() > table[srcNode].getTimeInt() + calculateTime(currentNode.distance))
         {
-            cout << "change\n";
             table[currentNode.nodeName].setDistance(table[srcNode].getDistance() + currentNode.distance);
             table[currentNode.nodeName].setParent(srcNode);
             table[currentNode.nodeName].setNodeVehicle(this);
             table[currentNode.nodeName].setCost(table[srcNode].getCost() + calculateCost(currentNode.distance));
             table[currentNode.nodeName].setTime(Time(table[srcNode].getTimeInt() + calculateTime(currentNode.distance)));
+            minHeap.push({currentNode.nodeName,table[srcNode].getTimeInt() + calculateTime(currentNode.distance)});
         }
     }
 }
 
-void Vehicle::calculateMinCost (unordered_map<string,NodeInfo>& table, const std::string& srcNode)
+void Vehicle::calculateMinCost (unordered_map<string,NodeInfo>& table,priority_queue<NodeNeighbour,vector<NodeNeighbour>,greater<NodeNeighbour>>&minHeap, const std::string& srcNode)
 {
     unordered_set<NodeNeighbour,NodeNeighbour::myHash> distanceSet;
     distanceFromSrc(distanceSet,srcNode);
@@ -128,6 +127,7 @@ void Vehicle::calculateMinCost (unordered_map<string,NodeInfo>& table, const std
             table[currentNode.nodeName].setNodeVehicle(this);
             table[currentNode.nodeName].setCost(table[srcNode].getCost() + calculateCost(currentNode.distance));
             table[currentNode.nodeName].setTime(Time(table[srcNode].getTimeInt() + calculateTime(currentNode.distance)));
+             minHeap.push({currentNode.nodeName,table[srcNode].getCost() + calculateCost(currentNode.distance)});
         }    
 }
 
